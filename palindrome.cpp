3546efa93e060e16015858f67a567c6204d5d017
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-29 17:26:25 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        ListNode* cur = head;
+        while(cur)
+        {
+            v.push_back(cur->val);
+            cur = cur->next;
+        }
+        int i;
+        for(i=0; i<v.size()/2; i++)
+        {
+            if(v[i]!= v[v.size()-1-i]) return false;
+        }
+        return true;
+    }
+};
